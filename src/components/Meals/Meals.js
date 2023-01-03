16cb5eae4f094c0d7d2b628e36733513c3e5eb72
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-01-03 00:11:41 -0300

    Initial commit

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AvaibleMeals from "./AvaibleMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <>
+      <MealsSummary />
+      <AvaibleMeals />
+    </>
+  );
+};
+
+export default Meals;
