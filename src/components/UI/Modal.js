fbaaf6c2fe33c434252bdc7c4fd3dac2d032c490
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-01-03 23:02:28 -0300

    Creating cart context, modal and logic

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { ReactDOM } from "react-dom";
+import  ReactDOM  from "react-dom";
 import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={props.onClose} />;
 };
 const ModalOverlay = (props) => {
   return (
@@ -17,7 +17,7 @@ const portalElement = document.getElementById("overlays");
 const Modal = (props) => {
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose} />, portalElement)}
       {ReactDOM.createPortal(
         <ModalOverlay>{props.children}</ModalOverlay>,
         portalElement
